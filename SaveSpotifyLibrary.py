cdcc7d3107515815cf75971b7107092b7ded4ef3
Claudio Pizarro Salcedo <claudito48@hotmail.de>
2023-01-20 10:46:18 +0100

    minor changes

--- SaveSpotifyLibrary.py
+++ SaveSpotifyLibrary.py
@@ -15,7 +15,7 @@ from slugify import slugify
 
 # CLIENT ID: 6200f2eac11e430a989056cebd09cd1d
 # CLIENT SECRET: 18a76a04435f455ab08b9def12b2587e
-# USER ID: https://open.spotify.com/user/claudito48?si=3ddbf5a88faf4343 
+# USER ID: https://open.spotify.com/user/claudito48?si=3ddbf5a88faf4343
 
 def iterate_playlist(spotify: spotipy.Spotify, output_file: io.TextIOWrapper, playlist):
   print(f"Indexing {playlist['name']}...")
@@ -49,7 +49,7 @@ def iterate_playlist(spotify: spotipy.Spotify, output_file: io.TextIOWrapper, pl
       output_file.write(f" - {track['name']} - {artists_str} \n")
 
     current_offset += len(tracks)
-    
+
     if len(tracks) < tracks_limit:
       break
 
@@ -79,21 +79,21 @@ def iterate_playlists(spotify: spotipy.Spotify):
 
     # otherwise accumulate the length for pagination
     current_offset += len(playlists)
-        
+
     # if the len of the 'items' array is less than the limit, we must have hit the end, so break
     if len(playlists) < playlists_limit:
       break
 
   print(f"Indexed {current_offset} playlists")
 
+# Example main (This is my spotify username tho feel free to browse my fire lists haha)
 def main():
   username = "claudito48"
-
-  scope = 'playlist-read-private' 
+  scope = 'playlist-read-private'
   client_id = '6200f2eac11e430a989056cebd09cd1d'
   client_secret = '18a76a04435f455ab08b9def12b2587e'
   redirect_uri = 'https://google.com/'
-  
+
   auth_man = spotipy.oauth2.SpotifyOAuth(
     client_id=client_id,
     client_secret=client_secret,
@@ -102,7 +102,7 @@ def main():
     username=username,
     open_browser=False,
   )
-  
+
   # Erase cache and prompt for user permissions
   try:
       token = util.prompt_for_user_token(
@@ -115,11 +115,11 @@ def main():
         oauth_manager=auth_man,
         show_dialog=False,
       )
-      
+
   # Create spotifyObject
-  
+
   spotify = spotipy.Spotify(auth=token)
-  
+
   try:
     os.mkdir("playlists")
   except:
